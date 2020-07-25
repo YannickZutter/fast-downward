4d2df696378c692ff5c2c41a321faa9e1ce0cb7d
Yannick Zutter <yannick.zutter@stud.unibas.ch>
2020-07-25 16:30:45 +0200

    added first working version of relaxed sg

--- src/search/task_utils/relaxed_successor_generator.cc
+++ src/search/task_utils/relaxed_successor_generator.cc
@@ -13,245 +13,94 @@
 #include "../utils/logging.h"
 #include "../utils/timer.h"
 
-
-
+#include <algorithm>
+#include <cassert>
+#include <cstddef>
+#include <unordered_map>
+#include <vector>
 
 using namespace std;
 
 namespace successor_generator{
 
-    Fact::Fact()
-            : cost(-1),
-              reached_by(NO_OP),
-              is_goal(false),
-              marked(false),
-              num_precondition_occurences(-1) {
-    }
-    RelaxedSuccessorGenerator::RelaxedSuccessorGenerator() {
 
-    }
+    RelaxedSuccessorGenerator::RelaxedSuccessorGenerator() = default;
 
     RelaxedSuccessorGenerator::~RelaxedSuccessorGenerator() = default;
 
     void RelaxedSuccessorGenerator::initialize(const TaskProxy &task_proxy) {
 
-        //setting propositions
         facts.resize(task_properties::get_num_facts(task_proxy));
-        // setting offset
-        VariablesProxy variables = task_proxy.get_variables();
-        fact_offsets.reserve(variables.size());
-        PropID offset = 0;
-        for(VariableProxy var : variables){
+        counter.resize(task_proxy.get_operators().size());
+
+        for(OperatorProxy op : task_proxy.get_operators()){
+            operators.push_back(op);
+        }
+
+        int offset = 0;
+        for(VariableProxy var : task_proxy.get_variables()){
             fact_offsets.push_back(offset);
             offset += var.get_domain_size();
         }
-        assert(offset == static_cast<int>(facts.size()));
+        for(VariableProxy var : task_proxy.get_variables()){
+            for(int i = 0; i < var.get_domain_size(); i++){
 
-        //setting unary operators for operators and axioms
-        unary_operators.reserve(task_properties::get_num_total_effects(task_proxy));
-        for(OperatorProxy op : task_proxy.get_operators()){
-            build_unary_operators(op);
-        }
-        for(OperatorProxy axiom : task_proxy.get_axioms()){
-            build_unary_operators(axiom);
-        }
+                FactProxy val = var.get_fact(i);
 
-        //simplify unary operators
-        utils::Timer simplify_timer;
-        simplify();
-        utils::g_log << "time to simplify: " << simplify_timer << endl;
+                for(OperatorProxy op : operators){
+                    PreconditionsProxy precons = op.get_preconditions();
 
-        //cross-reference unary operators
+                    for(int j = 0; j < int(precons.size()); j++){
+                        FactPair pair = precons[j].get_pair();
+                        if(pair.var == var.get_id()){
+                            if(pair.value == val.get_value()){
+                                facts[get_fact_id(var, val)].emplace_back(OperatorID(op.get_id()));
+                            }
+                        }
 
-        vector<vector<OpID>> precondition_of_vectors(facts.size());
-
-        int num_unary_ops = unary_operators.size();
-
-        for(OpID op_id = 0; op_id < num_unary_ops; op_id++){
-            for(PropID precond : get_preconditions(op_id)){
-                precondition_of_vectors[precond].push_back(op_id);
+                    }
+                    counter[op.get_id()] = op.get_preconditions().size();
+                }
             }
         }
 
-        int num_facts = facts.size();
-
-        //counter.reserve(num_facts);
-        for(PropID prop_id = 0; prop_id < num_facts; prop_id++){
-            auto precondition_of_vec = move(precondition_of_vectors[prop_id]);
-            facts[prop_id].precondition_of = precondition_of_pool.append(precondition_of_vec);
-            facts[prop_id].num_precondition_occurences = precondition_of_vec.size();
-            //counter[prop_id] = precondition_of_vec.size();
-        }
-
     }
 
-    void RelaxedSuccessorGenerator::reset_counter(vector<int> &cnt){
-
-        int s = cnt.size();
-        assert(s == facts.size());
-
-
-        for(int i = 0; i < s; i++){
-            cnt[i] = facts[i].num_precondition_occurences;
-        }
-    }
     void RelaxedSuccessorGenerator::generate_applicable_ops(const State &state, vector<OperatorID> &applicable_ops) const{
 
-        vector<int> counter;
-        int facts_size = facts.size();
-        int counter_size = counter.size();
-        counter.reserve(facts_size);
-        for(int i = 0; i < facts_size; i++){
-            counter[i] = facts[i].num_precondition_occurences;
-        }
+        vector<int> cnt = counter;
 
         for(FactProxy fact : state){
-            PropID prop = get_prop_id(fact);
-            counter[prop]--;
-        }
-        for(int i = 0; i < counter_size; i++){
-            if(counter[i] == 0){
-                applicable_ops.push_back(OperatorID(i));
+            int var = fact.get_pair().var;
+            int val = fact.get_pair().value;
+
+            for(OperatorID op_id : facts[get_fact_id(var, val)]){
+                cnt[op_id.get_index()]--;
             }
         }
 
+        for(int i = 0; i < int(cnt.size()); i++){
+            if(cnt[i] == 0){
+                applicable_ops.push_back(OperatorID(operators[i].get_id()));
+            }
+        }
     }
 
     void RelaxedSuccessorGenerator::generate_applicable_ops(const GlobalState &state, vector<OperatorID> &applicable_ops) const{
 
         generate_applicable_ops(state.unpack(), applicable_ops);
-
     }
 
-    void RelaxedSuccessorGenerator::build_unary_operators(const OperatorProxy &op) {
-        int op_no = op.is_axiom() ? -1 : op.get_id();
-        int base_cost = op.get_cost();
-        vector<PropID> precondition_props;
-        PreconditionsProxy preconditions = op.get_preconditions();
-        precondition_props.reserve(preconditions.size());
-        for (FactProxy precondition : preconditions) {
-            precondition_props.push_back(get_prop_id(precondition));
-        }
-        for (EffectProxy effect : op.get_effects()) {
-            PropID effect_prop = get_prop_id(effect.get_fact());
-            EffectConditionsProxy eff_conds = effect.get_conditions();
-            precondition_props.reserve(preconditions.size() + eff_conds.size());
-            for (FactProxy eff_cond : eff_conds) {
-                precondition_props.push_back(get_prop_id(eff_cond));
-            }
 
-            // The sort-unique can eventually go away. See issue497.
-            vector<PropID> preconditions_copy(precondition_props);
-            utils::sort_unique(preconditions_copy);
-            array_pool::ArrayPoolIndex precond_index =
-                    preconditions_pool.append(preconditions_copy);
-            unary_operators.emplace_back(
-                    preconditions_copy.size(), precond_index, effect_prop,
-                    op_no, base_cost);
-            precondition_props.erase(precondition_props.end() - eff_conds.size(), precondition_props.end());
-        }
-    }
-
-    void RelaxedSuccessorGenerator::simplify() {
-
-#ifndef NDEBUG
-        int num_ops = unary_operators.size();
-        for (OpID op_id = 0; op_id < num_ops; ++op_id)
-            assert(utils::is_sorted_unique(get_preconditions_vector(op_id)));
-#endif
-
-        const int MAX_PRECONDITIONS_TO_TEST = 5;
-
-        utils::g_log << "Simplifying " << unary_operators.size() << " unary operators..." << flush;
-
-        using Key = pair<vector<PropID>, PropID>;
-        using Value = pair<int, OpID>;
-        using Map = utils::HashMap<Key, Value>;
-        Map unary_operator_index;
-        unary_operator_index.reserve(unary_operators.size());
-
-        for (size_t op_no = 0; op_no < unary_operators.size(); ++op_no) {
-            const UnaryOperator &op = unary_operators[op_no];
-
-            Key key(get_preconditions_vector(op_no), op.effect);
-            Value value(op.base_cost, op_no);
-            auto inserted = unary_operator_index.insert(
-                    make_pair(move(key), value));
-            if (!inserted.second) {
-                // We already had an element with this key; check its cost.
-                Map::iterator iter = inserted.first;
-                Value old_value = iter->second;
-                if (value < old_value)
-                    iter->second = value;
-            }
-        }
-
-        Key dominating_key;
-
-        auto is_dominated = [&](const UnaryOperator &op) {
-
-            OpID op_id = get_op_id(op);
-            int cost = op.base_cost;
-
-            const vector<PropID> precondition = get_preconditions_vector(op_id);
-
-            if (unary_operator_index[make_pair(precondition, op.effect)].second != op_id)
-                return true;
-
-            if (op.num_preconditions > MAX_PRECONDITIONS_TO_TEST) {
-
-                return false;
-            }
-
-            vector<PropID> &dominating_precondition = dominating_key.first;
-            dominating_key.second = op.effect;
-
-            int powerset_size = (1 << precondition.size()) - 1;
-            for (int mask = 0; mask < powerset_size; ++mask) {
-                dominating_precondition.clear();
-                for (size_t i = 0; i < precondition.size(); ++i)
-                    if (mask & (1 << i))
-                        dominating_precondition.push_back(precondition[i]);
-                Map::iterator found = unary_operator_index.find(dominating_key);
-                if (found != unary_operator_index.end()) {
-                    Value dominator_value = found->second;
-                    int dominator_cost = dominator_value.first;
-                    if (dominator_cost <= cost)
-                        return true;
-                }
-            }
-            return false;
-        };
-
-        unary_operators.erase(
-                remove_if(
-                        unary_operators.begin(),
-                        unary_operators.end(),
-                        is_dominated),
-                unary_operators.end());
-
-        utils::g_log << " done! [" << unary_operators.size() << " unary operators]" << endl;
-    }
-
-    PropID RelaxedSuccessorGenerator::get_prop_id(int var, int value) const {
+    int RelaxedSuccessorGenerator::get_fact_id(int var, int value) const {
         return fact_offsets[var] + value;
     }
 
-    PropID RelaxedSuccessorGenerator::get_prop_id(const FactProxy &fact) const {
-        return get_prop_id(fact.get_variable().get_id(), fact.get_value());
+    int RelaxedSuccessorGenerator::get_fact_id(const FactProxy &fact) const {
+        return get_fact_id(fact.get_variable().get_id(), fact.get_value());
     }
-
-    const Fact *RelaxedSuccessorGenerator::get_proposition(
-            int var, int value) const {
-        return &facts[get_prop_id(var, value)];
-    }
-
-    Fact *RelaxedSuccessorGenerator::get_proposition(int var, int value) {
-        return &facts[get_prop_id(var, value)];
-    }
-
-    Fact *RelaxedSuccessorGenerator::get_proposition(const FactProxy &fact) {
-        return get_proposition(fact.get_variable().get_id(), fact.get_value());
+    int RelaxedSuccessorGenerator::get_fact_id(VariableProxy var, FactProxy value) const {
+        return get_fact_id(var.get_id(), value.get_value());
     }
 
     static shared_ptr<successor_generator::SuccessorGeneratorBase> _parse(OptionParser &parser) {
@@ -263,13 +112,4 @@ namespace successor_generator{
 
     static Plugin<successor_generator::SuccessorGeneratorBase> _plugin("relaxed", _parse);
 
-    UnaryOperator::UnaryOperator(
-            int num_preconditions, array_pool::ArrayPoolIndex preconditions,
-            PropID effect, int operator_no, int base_cost)
-            : effect(effect),
-              base_cost(base_cost),
-              num_preconditions(num_preconditions),
-              preconditions(preconditions),
-              operator_no(operator_no) {
-    }
 }
--- src/search/task_utils/relaxed_successor_generator.h
+++ src/search/task_utils/relaxed_successor_generator.h
@@ -10,89 +10,13 @@ namespace successor_generator{
     class GeneratorBase;
 
     using namespace std;
-    using PropID = int;
-    using OpID = int;
-
-    const OpID NO_OP = -1;
-
-    struct Fact {
-        Fact();
-        int cost; // used for h^max cost or h^add cost
-        // TODO: Make sure in constructor that reached_by does not overflow.
-        OpID reached_by : 30;
-        /* The following two variables are conceptually bools, but Visual C++ does
-           not support packing ints and bools together in a bitfield. */
-        unsigned int is_goal : 1;
-        unsigned int marked : 1; // used for preferred operators of h^add and h^FF
-        int num_precondition_occurences;
-        array_pool::ArrayPoolIndex precondition_of;
-    };
-
-    static_assert(sizeof(Fact) == 16, "Fact has wrong size");
-
-    struct UnaryOperator {
-        UnaryOperator(int num_preconditions, array_pool::ArrayPoolIndex preconditions, PropID effect,int operator_no, int base_cost);
-        int cost; // Used for h^max cost or h^add cost;
-        // includes operator cost (base_cost)
-        int unsatisfied_preconditions;
-        PropID effect;
-        int base_cost;
-        int num_preconditions;
-        array_pool::ArrayPoolIndex preconditions;
-        int operator_no; // -1 for axioms; index into the task's operators otherwise
-    };
-
 
     class RelaxedSuccessorGenerator : public successor_generator::SuccessorGeneratorBase{
 
-        vector<PropID> fact_offsets;
-        vector<Fact> facts;
-        //vector<int> counter;
-        vector<UnaryOperator> unary_operators;
-
-        array_pool::ArrayPool preconditions_pool;
-        array_pool::ArrayPool precondition_of_pool;
-
-        void build_unary_operators(const OperatorProxy &op);
-        void simplify();
-
-
-        array_pool::ArrayPoolSlice get_preconditions(OpID op_id) const {
-            const UnaryOperator &op = unary_operators[op_id];
-            return preconditions_pool.get_slice(op.preconditions, op.num_preconditions);
-        }
-
-        vector<PropID> get_preconditions_vector(OpID op_id) const {
-            auto view = get_preconditions(op_id);
-            return std::vector<PropID>(view.begin(), view.end());
-        }
-
-        PropID get_prop_id(const Fact &prop) const {
-            PropID prop_id = &prop - facts.data();
-            assert(utils::in_bounds(prop_id, facts));
-            return prop_id;
-        }
-
-        OpID get_op_id(const UnaryOperator &op) const {
-            OpID op_id = &op - unary_operators.data();
-            assert(utils::in_bounds(op_id, unary_operators));
-            return op_id;
-        }
-
-        PropID get_prop_id(int var, int value) const;
-        PropID get_prop_id(const FactProxy &fact) const;
-
-        Fact *get_proposition(PropID prop_id) {
-            return &facts[prop_id];
-        }
-        UnaryOperator *get_operator(OpID op_id) {
-            return &unary_operators[op_id];
-        }
-
-        const Fact *get_proposition(int var, int value) const;
-        Fact *get_proposition(int var, int value);
-        Fact *get_proposition(const FactProxy &fact);
-
+        vector<int> fact_offsets;
+        vector<vector<OperatorID>> facts;
+        vector<OperatorProxy> operators;
+        vector<int> counter;
 
 
     public:
@@ -100,15 +24,18 @@ namespace successor_generator{
 
         virtual ~RelaxedSuccessorGenerator();
 
+
         void initialize(const TaskProxy &task_proxy) override;
 
         void generate_applicable_ops(const State &state, std::vector<OperatorID> &applicable_ops) const override ;
 
         void generate_applicable_ops(const GlobalState &state, std::vector<OperatorID> &applicable_ops) const override ;
 
-        void reset_counter(vector<int> &cnt);
+        int get_fact_id(int var, int value) const;
 
+        int get_fact_id(const FactProxy &fact) const;
 
+        int get_fact_id(VariableProxy var, FactProxy value) const;
 
     };
 }
