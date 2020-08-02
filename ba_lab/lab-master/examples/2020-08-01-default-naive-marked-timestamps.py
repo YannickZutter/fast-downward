5e9228bc2d0797eba07bc72d3bc5187e6231bc56
YannickZutter <yannick.zutter@stud.unibas.ch>
2020-08-02 04:20:06 +0200

    aqs

--- ba_lab/lab-master/examples/2020-08-01-default-naive-marked-timestamps.py
+++ ba_lab/lab-master/examples/2020-08-01-default-naive-marked-timestamps.py
@@ -18,20 +18,19 @@ ATTRIBUTES = ["coverage", "error", "expansions", "total_time"]
 NODE = platform.node()
 if NODE.endswith(".scicore.unibas.ch") or NODE.endswith(".cluster.bc2.ch"):
     # Create bigger suites with suites.py from the downward-benchmarks repo.
-    SUITE = ["gripper:prob01"]
-    ENV = BaselSlurmEnvironment(email="yannick.zutter@stud.unibas.ch")
+    SUITE = ["gripper:prob01.pddl"]
     REPO = os.path.expanduser("~/fast-downward")
+    ENV = BaselSlurmEnvironment(email="yannick.zutter@stud.unibas.ch")
 else:
-    SUITE = ["gripper:prob01"]
-    ENV = LocalEnvironment(processes=2)
+    SUITE = ["gripper:prob01.pddl"]
     REPO = os.path.expanduser("~/CLionProjects/fast-downward")
+    ENV = LocalEnvironment(processes=2)
 # Use path to your Fast Downward repository.
-
 BENCHMARKS_DIR = os.path.expanduser("~/benchmarks")
 # If REVISION_CACHE is None, the default ./data/revision-cache is used.
 REVISION_CACHE = os.environ.get("DOWNWARD_REVISION_CACHE")
 VCS = cached_revision.get_version_control_system(REPO)
-REV = "c0beae66e998003156b20f90678ef0c21287de0d"
+REV = "8778a0c4ab6f9a2173603843e032f01603fd8930"
 
 exp = FastDownwardExperiment(environment=ENV, revision_cache=REVISION_CACHE)
 
@@ -42,10 +41,10 @@ exp.add_parser(exp.SINGLE_SEARCH_PARSER)
 exp.add_parser(exp.PLANNER_PARSER)
 
 exp.add_suite(BENCHMARKS_DIR, SUITE)
-exp.add_algorithm("astar blind default", REPO, REV, ["--search", "astar(blind(), sg=default)"])
-exp.add_algorithm("astar blind naive", REPO, REV, ["--search", "astar(blind(), sg=naive)"])
-exp.add_algorithm("astar blind marked", REPO, REV, ["--search", "astar(blind(), sg=marked)"])
-exp.add_algorithm("astar blind timestamps", REPO, REV, ["--search", "astar(blind(), sg=timestamps)"])
+exp.add_algorithm("default", REPO, REV, ["--search", "astar(blind(), sg = default)"])
+exp.add_algorithm("naive", REPO, REV, ["--search", "astar(blind(), sg = naive)"])
+exp.add_algorithm("marked", REPO, REV, ["--search", "astar(blind(), sg = marked)"])
+exp.add_algorithm("timestamps", REPO, REV, ["--search", "astar(blind(), sg = timestamps)"])
 
 # Add step that writes experiment files to disk.
 exp.add_step("build", exp.build)
@@ -59,6 +58,12 @@ exp.add_fetcher(name="fetch")
 
 # Add report step (AbsoluteReport is the standard report).
 exp.add_report(AbsoluteReport(attributes=ATTRIBUTES), outfile="report.html")
+
+# Add scatter plot report step.
+exp.add_report(
+    ScatterPlotReport(attributes=["expansions"], filter_algorithm=["blind", "lmcut"]),
+    outfile="scatterplot.png",
+)
 
 # Parse the commandline and show or run experiment steps.
 exp.run_steps()
