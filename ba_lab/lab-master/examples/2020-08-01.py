4326b46801612049ce7d0b0ee7af46b8a5060993
YannickZutter <yannick.zutter@stud.unibas.ch>
2020-08-03 02:32:19 +0200

    sdf

--- ba_lab/lab-master/examples/2020-08-01.py
+++ ba_lab/lab-master/examples/2020-08-01.py
@@ -29,7 +29,7 @@ BENCHMARKS_DIR = os.path.expanduser("~/benchmarks")
 # If REVISION_CACHE is None, the default ./data/revision-cache is used.
 REVISION_CACHE = os.environ.get("DOWNWARD_REVISION_CACHE")
 VCS = cached_revision.get_version_control_system(REPO)
-REV = "4d2df696378c692ff5c2c41a321faa9e1ce0cb7d"
+REV = "1639684f8d58bf79fa4d4c2f7f0685c0c867788f"
 
 exp = FastDownwardExperiment(environment=ENV, revision_cache=REVISION_CACHE)
 
