205d9e4d68a141e94167b487b3c43f8f93f87cde
YannickZutter <yannick.zutter@stud.unibas.ch>
2020-08-03 02:12:03 +0200

    sdf

--- ba_lab/lab-master/examples/2020-08-01.py
+++ ba_lab/lab-master/examples/2020-08-01.py
@@ -13,25 +13,24 @@ from lab import cached_revision
 from lab.environments import BaselSlurmEnvironment, LocalEnvironment
 
 
-ATTRIBUTES = ["coverage", "expansions", "total_time", "expansions_until_last_jump", "search_time", "cost", "error", "run_dir"]
+ATTRIBUTES = ["coverage", "expansions", "total_time", "expansions_until_last_jump", "search_time", "cost", "error"]
 
 NODE = platform.node()
 if NODE.endswith(".scicore.unibas.ch") or NODE.endswith(".cluster.bc2.ch"):
     # Create bigger suites with suites.py from the downward-benchmarks repo.
-    SUITE = ["gripper"]
-    ENV = BaselSlurmEnvironment(email="yannick.zutter@stud.unibas.ch")
+    SUITE = ["gripper:prob01.pddl"]
     REPO = os.path.expanduser("~/fast-downward")
+    ENV = BaselSlurmEnvironment(email="yannick.zutter@stud.unibas.ch")
 else:
-    SUITE = ["depot:p01.pddl", "gripper:prob01.pddl", "gripper:prob02.pddl"]
-    ENV = LocalEnvironment(processes=2)
+    SUITE = ["gripper:prob01.pddl"]
     REPO = os.path.expanduser("~/CLionProjects/fast-downward")
+    ENV = LocalEnvironment(processes=2)
 # Use path to your Fast Downward repository.
-
 BENCHMARKS_DIR = os.path.expanduser("~/benchmarks")
 # If REVISION_CACHE is None, the default ./data/revision-cache is used.
 REVISION_CACHE = os.environ.get("DOWNWARD_REVISION_CACHE")
 VCS = cached_revision.get_version_control_system(REPO)
-REV = "b083700c6d7be4202dd2c1a20e2c3a57e5ef4f32"
+REV = "cab80210e423b4fb6bb88ae48c016f39c80facd4"
 
 exp = FastDownwardExperiment(environment=ENV, revision_cache=REVISION_CACHE)
 
@@ -42,9 +41,10 @@ exp.add_parser(exp.SINGLE_SEARCH_PARSER)
 exp.add_parser(exp.PLANNER_PARSER)
 
 exp.add_suite(BENCHMARKS_DIR, SUITE)
-exp.add_algorithm("astar blind default", REPO, REV, ["--search", "astar(blind(), sg=default)"])
-exp.add_algorithm("astar blind naive", REPO, REV, ["--search", "astar(blind(), sg=naive)"])
-exp.add_algorithm("astar blind relaxed", REPO, REV, ["--search", "astar(blind(), sg=relaxed)"])
+exp.add_algorithm("default", REPO, REV, ["--search", "astar(blind(), sg = default)"])
+exp.add_algorithm("naive", REPO, REV, ["--search", "astar(blind(), sg = naive)"])
+exp.add_algorithm("marked", REPO, REV, ["--search", "astar(blind(), sg = marked)"])
+exp.add_algorithm("timestamps", REPO, REV, ["--search", "astar(blind(), sg = timestamps)"])
 
 # Add step that writes experiment files to disk.
 exp.add_step("build", exp.build)
